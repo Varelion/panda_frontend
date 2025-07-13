961887a80da8152a32bdcc9499dc9b0bfa2d1c22
Varelion <business.8508@gmail.com>
2025-07-13 12:28:11 -0400

    Add bug comment

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -2,9 +2,8 @@
 //#region src/components/dashboard/index.js
 // --------------------------
 
-export {default as Dashboard} from './Dashboard'
-
+export { default } from './Dashboard';
 
 // --------------------------
-//#endregion src/components/dasjboard/index.js
+//#endregion src/components/dashboard/index.js
 // --------------------------
